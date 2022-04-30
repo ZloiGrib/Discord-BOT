d82943dd70951cb8bc5b0f93d1537f9bc7a8f850
Илья Меткурбанов <metkurbanovila@gmail.com>
2022-04-30 15:25:25 +0300

    goroda

--- Discord-BOT1.py
+++ Discord-BOT1.py
@@ -6,7 +6,7 @@ from config import settings
 
 # os.chdir('/home/peoples/discord-bot')
 
-def parse_city_json(json_file='russia.json'):
+def parse_city_json(json_file='Goroda.json'):
     content = {}
     p_obj = None
     try:
