31973ff6bb187c3b92f548538a746de3a61be325
ilija04022006 <metkurbanovila@gmail.com>
2022-05-10 19:12:20 +0300

    123READMI

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
 
-    'token': 'OTY5OTAwMTY1OTI0Mjc0MTk2.Ym0H6w.vWz1gIO-pXIrrw3FswyiGJ2C6Hc',
+    'token': 'OTY5OTAwMTY1OTI0Mjc0MTk2.G73gxG.f56Z9UvP72l7vkEoqLWd-MALYuZeGSbHPsePV8',
 
     'bot': 'Bot-Govorilka',
 
